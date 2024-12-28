1fe75d0a0217c4cc9d169cc97dbe912a7bfac98c
Bart Slinger <bartslinger@gmail.com>
2024-12-28 17:42:01 +0800

    read who-am-i in a loop with dma

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ use panic_halt as _;
 mod app {
     // use cortex_m_semihosting::{debug, hprintln};
     use rtic_monotonics::systick::prelude::*;
+    use stm32f4xx_hal::dma::traits::StreamISR;
     use stm32f4xx_hal::hal_02::digital::v2::OutputPin;
-    use stm32f4xx_hal::ReadFlags;
+    use stm32f4xx_hal::{ClearFlags, Listen, ReadFlags};
 
     systick_monotonic!(Mono, 1000);
 
@@ -34,6 +35,10 @@ mod app {
             None,
         READ_WHO_AM_I: [u8; 2] = [0x75 | 0x80, 0x00],
         ANSWER: [u8; 2] = [0xAA, 0xAA],
+        TX_BUFFER_1: [u8; 2] = [0x75 | 0x80, 0x00],
+        TX_BUFFER_2: [u8; 2] = [0x75 | 0x80, 0x00],
+        RX_BUFFER_1: [u8; 2] = [0xAA, 0xBB],
+        RX_BUFFER_2: [u8; 2] = [0xAA, 0xBB],
     ])]
     fn init(cx: init::Context) -> (Shared, Local) {
         use stm32f4xx_hal::prelude::*; // for .freeze() and constrain()
@@ -119,7 +124,7 @@ mod app {
                 polarity: stm32f4xx_hal::spi::Polarity::IdleHigh,
                 phase: stm32f4xx_hal::spi::Phase::CaptureOnSecondTransition,
             },
-            100.kHz(),
+            1.MHz(),
             &clocks,
         );
 
@@ -187,49 +192,63 @@ mod app {
         delay.delay_us(300);
 
         // try a two-step who-am-i
+        let start = dwt.cyccnt.read();
         cs.set_low();
         let mut buf = [0x75 | 0x80];
         spi1.transfer_in_place(&mut buf).expect("IMU write failed");
         defmt::info!("was 0x75: {:02x}", buf[0]);
         let mut buf = [0x00];
-        delay.delay_ms(1);
         spi1.transfer_in_place(&mut buf).expect("IMU write failed");
         defmt::info!("who am i: {:02x}", buf[0]);
         cs.set_high();
+        let end = dwt.cyccnt.read();
+        let diff = end.wrapping_sub(start);
+        defmt::info!("SPI read cycle count: {}", diff);
+
+        // try a who-am-i with interrupts
+        let start = dwt.cyccnt.read();
+        cs.set_low();
+        let mut buf = [0x75 | 0x80, 0x00];
+        // spi1.listen(stm32f4xx_hal::spi::Event::RxNotEmpty | stm32f4xx_hal::spi::Event::TxEmpty);
 
         // try a who-am-i with dma
         let (tx, rx) = spi1.use_dma().txrx();
-        cs.set_low();
 
         let tx_stream = dma2.3;
         let rx_stream = dma2.0;
 
-        let start = dwt.cyccnt.read();
         let mut rx_transfer = stm32f4xx_hal::dma::Transfer::init_peripheral_to_memory(
             rx_stream,
             rx,
-            cx.local.ANSWER,
+            cx.local.RX_BUFFER_1,
             None,
             stm32f4xx_hal::dma::config::DmaConfig::default().memory_increment(true),
         );
-        rx_transfer.start(|rx| {
-            defmt::info!("rx transfer started");
-        });
         let mut tx_transfer = stm32f4xx_hal::dma::Transfer::init_memory_to_peripheral(
             tx_stream,
             tx,
-            cx.local.READ_WHO_AM_I,
+            cx.local.TX_BUFFER_1,
             None,
             stm32f4xx_hal::dma::config::DmaConfig::default().memory_increment(true),
         );
+
+        let start = dwt.cyccnt.read();
+        cs.set_low();
+        // starting rx_transfer before tx_transfer seems more robust
+        // (works with even large delay between the two calls)
+        rx_transfer.start(|_rx| {
+            defmt::info!("rx transfer started");
+        });
+        // delay.delay_ms(1000);
         tx_transfer.start(|_tx| {
             defmt::info!("tx transfer started");
         });
+
         let end = dwt.cyccnt.read();
         let diff = end.wrapping_sub(start);
         defmt::info!("DMA SPI start cycle count: {}", diff);
 
-        delay.delay_ms(1);
+        rx_transfer.wait();
         let flags = tx_transfer.flags();
         if flags.is_transfer_complete() {
             defmt::info!("transfer complete");
@@ -242,10 +261,60 @@ mod app {
         } else {
             defmt::info!("transfer not complete");
         }
-        let (a, b, c, d) = rx_transfer.release();
-        defmt::info!("ok got {:02x}", c[1]);
-
+        // let (a, b, c, d) = rx_transfer.release();
+        // defmt::info!("ok got {:02x}", c[1]);
         cs.set_high();
+        delay.delay_us(1);
+
+        cs.set_low();
+        // send another DMA transfer by swapping the buffers
+        let (prev_tx_buffer, _) = tx_transfer
+            .next_transfer(cx.local.TX_BUFFER_2)
+            .expect("no next");
+        let (prev_rx_buffer, _) = rx_transfer
+            .next_transfer(cx.local.RX_BUFFER_2)
+            .expect("no next");
+
+        defmt::info!(
+            "prev tx buffer: {:02x} {:02x}",
+            prev_tx_buffer[0],
+            prev_tx_buffer[1]
+        );
+        defmt::info!(
+            "prev rx buffer: {:02x} {:02x}",
+            prev_rx_buffer[0],
+            prev_rx_buffer[1],
+        );
+
+        rx_transfer.wait();
+        cs.set_high();
+        let mut ready_tx_buffer = prev_tx_buffer;
+        let mut ready_rx_buffer = prev_rx_buffer;
+        loop {
+            delay.delay_ms(500);
+            delay.delay_us(1);
+
+            cs.set_low();
+            let (prev_tx_buffer, _) = tx_transfer.next_transfer(ready_tx_buffer).expect("no next");
+            let (prev_rx_buffer, _) = rx_transfer.next_transfer(ready_rx_buffer).expect("no next");
+
+            defmt::info!(
+                "prev tx buffer: {:02x} {:02x}",
+                prev_tx_buffer[0],
+                prev_tx_buffer[1]
+            );
+            defmt::info!(
+                "prev rx buffer: {:02x} {:02x}",
+                prev_rx_buffer[0],
+                prev_rx_buffer[1],
+            );
+
+            rx_transfer.wait();
+            cs.set_high();
+            ready_tx_buffer = prev_tx_buffer;
+            ready_rx_buffer = prev_rx_buffer;
+        }
+
         // let mut total_samples = 0;
         // let mut fifo_buffer = [0u8; 16 * 10 + 1];
         // loop {
@@ -390,6 +459,11 @@ mod app {
         (&mut usb_dev, &mut serial).lock(|usb_dev, serial| {
             super::usb_poll(usb_dev, serial);
         });
+    }
+
+    #[task(binds = SPI1, priority = 2)]
+    fn spi_interrupt(cx: spi_interrupt::Context) {
+        defmt::info!("SPI interrupt");
     }
 }
 
