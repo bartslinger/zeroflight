666623cc85ee04b11ca8cfbea65df779a9aec5e2
Bart Slinger <bartslinger@gmail.com>
2025-01-01 15:35:02 +0800

    configure imu filters for 1kHz sampling

--- src/icm42688p.rs
+++ src/icm42688p.rs
@@ -15,6 +15,24 @@ impl<const P: char, const N: u8> CsPin for stm32f4xx_hal::gpio::Pin<P, N, Output
     }
 }
 
+const REG_WHO_AM_I: u8 = 0x75;
+const REG_PWR_MGMT0: u8 = 0x4E;
+const REG_FIFO_CONFIG: u8 = 0x16;
+const REG_FIFO_CONFIG1: u8 = 0x5F;
+const REG_INTF_CONFIG0: u8 = 0x4C;
+const REG_INTF_CONFIG1: u8 = 0x4D;
+const REG_SIGNAL_PATH_RESET: u8 = 0x4B;
+const REG_GYRO_CONFIG0: u8 = 0x4F;
+const REG_ACCEL_CONFIG0: u8 = 0x50;
+const REG_REG_BANK_SEL: u8 = 0x76;
+const REG_GYRO_CONFIG_STATIC2: u8 = 0x0B;
+const REG_GYRO_CONFIG_STATIC3: u8 = 0x0C;
+const REG_GYRO_CONFIG_STATIC4: u8 = 0x0D;
+const REG_GYRO_CONFIG_STATIC5: u8 = 0x0E;
+const REG_ACCEL_CONFIG_STATIC2: u8 = 0x03;
+const REG_ACCEL_CONFIG_STATIC3: u8 = 0x04;
+const REG_ACCEL_CONFIG_STATIC4: u8 = 0x05;
+
 pub(crate) struct Icm42688p<CS: CsPin> {
     spi: stm32f4xx_hal::spi::Spi<stm32f4xx_hal::pac::SPI1, false, u8>,
     cs: CS,
@@ -31,88 +49,92 @@ impl<CS: CsPin> Icm42688p<CS> {
         self.cs.set_low();
         delay.delay_us(10);
 
+        let whoami = self.read_register(REG_WHO_AM_I);
+        if whoami != 0x47 {
+            defmt::error!("IMU not found");
+            panic!("IMU not found");
+        }
+
         // disable power on accel and gyro for configuration (see datasheet 12.9)
-        self.cs.set_low();
-        let mut buf = [0x4E, 0x00];
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        self.cs.set_high();
-        // min 200us sleep recommended
+        self.write_register(REG_PWR_MGMT0, 0x00);
+        delay.delay_us(10);
+        self.write_register(REG_FIFO_CONFIG, 0x80); // FIFO_CONFIG STOP-on-full
+        delay.delay_us(10);
+        self.write_register(REG_FIFO_CONFIG1, 0x07); // FIFO_CONFIG1 enable temp, accel, gyro
+        delay.delay_us(10);
+        self.write_register(REG_INTF_CONFIG0, 0xF0); // big Endian, count records, hold last sample
+        delay.delay_us(10);
+        self.write_register(REG_SIGNAL_PATH_RESET, 0x02); // flush the FIFO
+        delay.delay_us(10);
+        self.write_register(REG_GYRO_CONFIG0, 0x06); // gyro 1kHz ODR
+        delay.delay_us(10);
+        self.write_register(REG_ACCEL_CONFIG0, 0x06); // accel 1kHz ODR
+        delay.delay_us(10);
+
+        self.write_register(REG_REG_BANK_SEL, 1);
+        delay.delay_us(10);
+        let aaf_enable = self.read_register(REG_GYRO_CONFIG_STATIC2);
+        delay.delay_us(10);
+        self.write_register(REG_GYRO_CONFIG_STATIC2, aaf_enable & !0x03); // enable not and AAF
+        delay.delay_us(10);
+        self.write_register(REG_GYRO_CONFIG_STATIC3, 6); // 258Hz gyro bandwith
+        delay.delay_us(10);
+        self.write_register(REG_GYRO_CONFIG_STATIC4, 36); // 258Hz gyro bandwith
+        delay.delay_us(10);
+        self.write_register(
+            REG_GYRO_CONFIG_STATIC5,
+            (10 << 4) & 0xF0, /* | ((36 >> 8) & 0x0F) */
+        ); // 258Hz gyro bandwith
+        delay.delay_us(10);
+
+        self.write_register(REG_REG_BANK_SEL, 2);
+        delay.delay_us(10);
+        self.write_register(REG_ACCEL_CONFIG_STATIC2, 5 << 1); // 213Hz accel bandwith
+        delay.delay_us(10);
+        self.write_register(REG_ACCEL_CONFIG_STATIC3, 25); // 213Hz accel bandwith
+        delay.delay_us(10);
+        self.write_register(
+            REG_ACCEL_CONFIG_STATIC4,
+            (10 << 4) & 0xF0, /* | ((25 >> 8) & 0x0F) */
+        ); // 213Hz accel bandwith
+
+        self.write_register(REG_REG_BANK_SEL, 0);
+        delay.delay_us(10);
+
+        /*
+          From Ardupilot:
+          fix for the "stuck gyro" issue, which affects all IxM42xxx
+          sensors. This disables the AFSR feature which changes the
+          noise sensitivity with angular rate. When the switch happens
+          (at around 100 deg/sec) the gyro gets stuck for around 2ms,
+          producing constant output which causes a DC gyro bias
+        */
+        let v = self.read_register(REG_INTF_CONFIG1);
+        delay.delay_us(10);
+        self.write_register(REG_INTF_CONFIG1, (v & 0x3F) | 0x40);
+        delay.delay_us(10);
+
+        self.write_register(REG_PWR_MGMT0, 0x0F); // enable power on accel and gyro
+                                                  // min 200us sleep recommended
         delay.delay_us(300);
+    }
 
-        // configure the FIFO
+    fn read_register(&mut self, reg: u8) -> u8 {
         self.cs.set_low();
-        let mut buf = [0x16, 0x80]; // FIFO_CONFIG STOP-on-full
+        let mut buf = [reg | 0x80, 0x00];
         self.spi
             .transfer_in_place(&mut buf)
             .expect("IMU write failed");
         self.cs.set_high();
-        delay.delay_us(300);
+        buf[1]
+    }
 
+    fn write_register(&mut self, reg: u8, value: u8) {
         self.cs.set_low();
-        let mut buf = [0x5F, 0x07]; // FIFO_CONFIG1 enable temp, accel, gyro
+        let mut buf = [reg, value];
         self.spi
             .transfer_in_place(&mut buf)
             .expect("IMU write failed");
-        self.cs.set_high();
-        delay.delay_us(300);
-
-        self.cs.set_low();
-        let mut buf = [0x4C, 0xF0]; // big Endian, count records, hold last sample
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        self.cs.set_high();
-        delay.delay_us(300);
-
-        self.cs.set_low();
-        let mut buf = [0x4B, 0x02]; // SIGNAL_PATH_RESET flush the FIFO
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        self.cs.set_high();
-        delay.delay_us(300);
-
-        // // read power bits
-        // self.cs.set_low();
-        // let mut buf = [0x4E | 0x80, 0x00];
-        // self.spi.transfer_in_place(&mut buf).expect("IMU write failed");
-        // self.cs.set_high();
-        // defmt::info!("Power bits: {:02x}", buf[1]);
-        // delay.delay_ms(15);
-
-        // set accel range
-        self.cs.set_low();
-        let mut buf = [0x50, (0x00 << 5) | (0x06 & 0x0F)];
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        self.cs.set_high();
-        delay.delay_ms(15);
-
-        // enable power on accel and gyro
-        self.cs.set_low();
-        let mut buf = [0x4E, 0x0F];
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        self.cs.set_high();
-        // min 200us sleep recommended
-        delay.delay_us(300);
-
-        // try a two-step who-am-i
-        self.cs.set_low();
-        let mut buf = [0x75 | 0x80];
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        defmt::info!("was 0x75: {:02x}", buf[0]);
-        let mut buf = [0x00];
-        self.spi
-            .transfer_in_place(&mut buf)
-            .expect("IMU write failed");
-        defmt::info!("who am i: {:02x}", buf[0]);
         self.cs.set_high();
     }
 
