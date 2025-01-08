1123371dbe37752e75940e074fbde9696cc987b9
Bart Slinger <bartslinger@gmail.com>
2025-01-08 14:40:29 +0800

    generalize crsf serial interrupt handler

--- src/hw_tasks/crsf.rs
+++ src/hw_tasks/crsf.rs
@@ -1,25 +1,24 @@
-use crate::boards::board::CrsfSerial;
-use stm32f4xx_hal::nb;
+use embedded_hal_nb::nb;
 
-pub(crate) fn crsf_interrupt_handler(
-    serial: &mut CrsfSerial,
+pub(crate) fn crsf_interrupt_handler<SERIAL>(
+    serial: &mut SERIAL,
     crsf_data_sender: &mut rtic_sync::channel::Sender<'static, u8, 64>,
-) {
-    use stm32f4xx_hal::prelude::*;
-    if serial.is_rx_not_empty() {
-        let byte = match serial.read() {
-            Ok(v) => v,
-            Err(nb::Error::Other(e)) => {
-                defmt::info!("Error in receiving crsf byte: {}", e as u32);
-                return;
-            }
-            Err(nb::Error::WouldBlock) => {
-                defmt::info!("Error in receiving crsf byte: would block");
-                return;
-            }
-        };
-        if let Err(_e) = crsf_data_sender.try_send(byte) {
-            defmt::error!("send error from crsf interrupt");
-        };
-    }
+) where
+    SERIAL: embedded_hal_nb::serial::Read,
+{
+    use embedded_hal_nb::serial::Error;
+    let byte = match serial.read() {
+        Ok(v) => v,
+        Err(nb::Error::Other(e)) => {
+            defmt::info!("Error in receiving crsf byte: {}", e.kind() as u32);
+            return;
+        }
+        Err(nb::Error::WouldBlock) => {
+            defmt::info!("Error in receiving crsf byte: would block");
+            return;
+        }
+    };
+    if let Err(_e) = crsf_data_sender.try_send(byte) {
+        defmt::error!("send error from crsf interrupt");
+    };
 }
